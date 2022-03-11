e12fbc5aea049c5f300add74df687d4411fcadf5
Damian <damiangawlas@gmail.com>
2022-03-11 01:27:05 +0100

    Listing NFTs from wallet

--- src/App.js
+++ src/App.js
@@ -14,6 +14,9 @@ import idl from './idl.json';
 import kp from './keypair.json';
 import {Connection, PublicKey, clusterApiUrl} from '@solana/web3.js';
 import {Program, Provider, web3} from '@project-serum/anchor';
+//import {useWalletNfts} from "@nfteyez/sol-rayz-react";
+import {getParsedNftAccountsByOwner, isValidSolanaAddress} from "@nfteyez/sol-rayz"; //resolveToWalletAddress
+import axios from "axios";
 import './App.css';
 
 // Constants
@@ -32,7 +35,7 @@ const arr = Object.values(kp._keypair.secretKey);
 const secret = new Uint8Array(arr);
 const baseAccount = web3.Keypair.fromSecretKey(secret);
 
-let nftCount = 0;
+let nftCount = 0, nftCounter = 0;
 
 const App = () => {
 
@@ -41,6 +44,7 @@ const App = () => {
     const [inputNameValue, setInputNameValue] = useState('');
     const [inputNftValue, setInputNftValue] = useState('');
     const [nftList, setNftList] = useState([]);
+    const [AllNftData, setAllNftData] = useState([]);
     const [userName, setUserName] = useState('');
 
     /*
@@ -76,6 +80,7 @@ const App = () => {
             console.error(error);
         }
     };
+
     /*
      * Let's define this method so our code doesn't break.
      * We will write the logic for this next!
@@ -96,6 +101,25 @@ const App = () => {
             console.log('Empty input name. Try again.');
         }
     };
+
+    const onInputNameChange = (event) => {
+        const {value} = event.target;
+        setInputNameValue(value);
+    };
+    const onInputNftChange = (event) => {
+        const {value} = event.target;
+        setInputNftValue(value);
+    };
+    const getProvider = () => {
+        const connection = new Connection(network, opts.preflightCommitment);
+        return new Provider(
+            connection, window.solana, opts.preflightCommitment,
+        );
+    };
+
+    /*
+     * NFT functions
+     */
     const saveNft = async () => {
         if (inputNftValue.length === 0) {
             console.log("No NFT link given!")
@@ -121,20 +145,6 @@ const App = () => {
             console.log("Error sending NFT:", error)
         }
     };
-    const onInputNameChange = (event) => {
-        const {value} = event.target;
-        setInputNameValue(value);
-    };
-    const onInputNftChange = (event) => {
-        const {value} = event.target;
-        setInputNftValue(value);
-    };
-    const getProvider = () => {
-        const connection = new Connection(network, opts.preflightCommitment);
-        return new Provider(
-            connection, window.solana, opts.preflightCommitment,
-        );
-    };
     const createNftAccount = async () => {
         try {
             const provider = getProvider();
@@ -154,16 +164,83 @@ const App = () => {
         } catch (error) {
             console.log("Error creating BaseAccount account:", error)
         }
-    }
+    };
+    const getNftList = async () => {
+        try {
+            const provider = getProvider();
+            const program = new Program(idl, programID, provider);
+            const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
+            const accountNftList = account.nftList;
+
+            var userNfts = accountNftList.filter(nft => {
+                return nft.userAddress.toString() === walletAddress
+            });
+            setNftList(userNfts)
+
+        } catch (error) {
+            console.log("Error in getNftList: ", error);
+            setNftList(null);
+        }
+    };
+    const getAllNftData = async () => {
+        try {
+            if (walletAddress) {
+                const provider = getProvider();
+                const connection = provider.connection;
+
+                /*
+                const { nfts, isLoading, error } = useWalletNfts({
+                    publicAddress: walletAddress,
+                    connection,
+                });
+                // */
+                //*
+                let ownerToken = provider.wallet.publicKey;
+
+                //const result = isValidSolanaAddress(ownerToken);
+                const nfts = await getParsedNftAccountsByOwner({
+                    publicAddress: ownerToken,
+                    connection: connection,
+                    serialization: true,
+                });
+                // console.log(nfts);
+                // setAllNftData(nfts);
+                return nfts;
+                // */
+            }
+        } catch (error) {
+            console.log(error);
+        }
+    };
+    const getNftTokenData = async () => {
+        try {
+            let nftData = await getAllNftData();
+            var data = Object.keys(nftData).map((key) => nftData[key]);                                                                    let arr = [];
+            let n = data.length;
+            for (let i = 0; i < n; i++) {
+                //console.log(data[i].data.uri);
+                let val = await axios.get(data[i].data.uri);
+                arr.push(val);
+            }
+            // console.log(arr);
+            setAllNftData(arr);
+        } catch (error) {
+            console.log(error);
+        }
+    };
 
     /*
-     * We want to render this UI when the user hasn't connected
-     * their wallet to our app yet.
+     * Containers to render
      */
     const renderUserNameContainer = () => (
         <div className="username-container">
             Hi {userName}!
         </div>
+    );
+    const renderNoDataInfo = () => (
+        <span className="nodata-container">
+            no data
+        </span>
     );
 
     const renderNotConnectedContainer = () => (
@@ -232,6 +309,34 @@ const App = () => {
                         ))}
                         </tbody>
                     </table>
+                    <table className="nft-table">
+                        <thead className="nft-item table-header">
+                        <tr>
+                            <th className="item-name">Name</th>
+                            <th className="item-default">Symbol</th>
+                            <th className="item-default">Description</th>
+                            <th className="item-price">Acquisition Price</th>
+                            {/*<th className="item-price">Mint</th>*/}
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <tr className="divider">
+                            <td>&nbsp;</td>
+                        </tr>
+                        {AllNftData.length === 0 && renderEmptyInfo()}
+                        {AllNftData.map(nft => (
+                            <tr className="nft-item" key={nftCounter += 1}>
+                                <td className="item-name"><img src={nft.data.image.toString()} alt={nft.data.name.toString()}/>
+                                    <span>{nft.data.name.toString()}</span></td>
+                                <td className="item-default">{!nft.data.symbol.toString() && renderNoDataInfo()}{nft.data.symbol.toString()}</td>
+                                <td className="item-default">{nft.data.description.toString()}</td>
+                                <td className="item-price">x SOL</td>
+                                {/*<td className="item-price">{nft.mint.toString()}</td>*/}
+                            </tr>
+
+                        ))}
+                        </tbody>
+                    </table>
                 </div>
             )
         }
@@ -256,29 +361,12 @@ const App = () => {
         return () => window.removeEventListener('load', onLoad);
     }, []);
 
-    const getNftList = async () => {
-        try {
-            const provider = getProvider();
-            const program = new Program(idl, programID, provider);
-            const account = await program.account.baseAccount.fetch(baseAccount.publicKey);
-            const accountNftList = account.nftList;
-
-            var userNfts = accountNftList.filter(nft => {
-                return nft.userAddress.toString() === walletAddress
-            });
-            setNftList(userNfts)
-
-        } catch (error) {
-            console.log("Error in getNftList: ", error);
-            setNftList(null);
-        }
-    };
-
 
     useEffect(() => {
         if (walletAddress) {
-            console.log('Fetching NFT list...');
+            //console.log('Fetching NFT list...');
             getNftList()
+            getNftTokenData()
         }
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [walletAddress]);
